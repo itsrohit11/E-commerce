0b3a1db1f299b82fb6300f427d70e005ed584bbc
itsrohit11 <officialrohitjain11@gmail.com>
2024-11-30 02:36:22 +0530

    Maincorousel

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigationbar from './Customer/Component/Navigation/Navigationbar';
+import Homepage from './Customer/Pages/HomePage/Homepage';
 
 function App() {
   return (
     <div className="">
       <Navigationbar/>
       <div>
-        Hello World
+      <Homepage/>
       </div>
     </div>
   );
 }
 
-export default Navigationbar;
+export default App;
---
+++ src/Customer/Pages/HomePage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarousel from '../../Component/HomeCarousel/MainCarousel'
+
+const Homepage = () => {
+  return (
+    <div>
+      <MainCarousel/>
+      <div>
+        otherSection
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
